7c3c7b9bf6bdfb553d0021da2708b6019e733f9b
unknown <frodriguezb@minsait.com>
2024-03-12 16:58:30 -0600

    Se carga proyecto vacío

--- src/main/java/com/indra/micros/MicrosApplication.java
+++ src/main/java/com/indra/micros/MicrosApplication.java
@@ -1,5 +1,4 @@
 package com.indra.micros;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/indra/micros/dominio/validadorArchivos.java
@@ -0,0 +1,5 @@
+package com.indra.micros.dominio;
+
+public class validadorArchivos {
+
+}
